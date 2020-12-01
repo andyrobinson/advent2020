900b341769861706b6122dbc6fdcf2107441fe14
Andy Robinson <andy.robinson01@bbc.co.uk>
2020-12-01 18:42:40 +0000

    Functional golf

--- src/main/scala/day1/Main.scala
+++ src/main/scala/day1/Main.scala
@@ -1,29 +1,23 @@
 package day1
 
 import scala.io.Source
-import scala.collection.mutable.Set
 
 object Main extends App {
-  println("Calculating ...")
-  val filename = "src/main/scala/day1/expenses.txt"
 
-  var numbers: Set[Int] = Set.empty[Int]
-  for (line <- Source.fromFile(filename).getLines) {
-    numbers += line.toInt
-  }
+  def search1(l: Seq[Int], target: Int) = l.find(_ == target)
+  def search2(l: Seq[Int], target: Int) = doit(l, target, search1)
 
-  numbers.foreach(i => {
-    numbers.find(j => i+j == 2020).map { answer =>
-      println ("Answer1: " + answer * i)
-    }
-  })
-
-  numbers.foreach(i => {
-    numbers.filter(j => i+j < 2020).foreach (j => {
-      numbers.find(k => i+j+k == 2020).map { answer =>
-        println ("Answer2" + ": " + answer * i * j)
-      }
-    })
-  })
+  def doit(num: Seq[Int], target : Int, fn: (Seq[Int], Int) => Option[Int]): Option[Int] =
+      num.foldLeft(Option.empty[Int], num.tail)((acc, i) => {acc match {
+        case (Some(result), _)  => (Some(result), Nil)
+        case (None, l) if l.nonEmpty => (fn(l, target - i).map(_*i), l.tail)
+        case (None, _) => (None, Nil)
+      }})._1
   
+  val filename = "expenses.txt"
+  val numbers: Seq[Int] = Source.fromResource(filename).getLines.map(_.toInt).toList
+
+  println("Answer1: " + doit(numbers, 2020, search1).get)
+  println("Answer2: " + doit(numbers, 2020, search2).get)
+
 }
